75603871e2053daba95336aba941ab1304805b4d
shouq <shouqali.v@gmail.com>
2025-03-12 17:38:01 +0300

    new application

--- app2.py
+++ app2.py
@@ -1,25 +1,15 @@
 import streamlit as st 
 
-st.title("Simple Calculator")
+st.title("Personal Information Form")
 
-num1 = st.number_input('Enter first number', value= 0.0)
-num2 = st.number_input('Enter second number', value= 0.0)
+name = st.text_input("Enter your name")
+age = st.number_input("Enter your age", 0, 120, 25)
+bio = st.text_area("write a short bio")
 
-operation = st.selectbox("choose what you want to do with the 2 numbers entered",["+","-","*","/"])
-
-# Calculate button
-if st.button("Calculate"):
-    if operation == "+":
-        result = num1 + num2
-    elif operation == "-":
-        result = num1 - num2
-    elif operation == "*":
-        result = num1 * num2
-    elif operation == "/":
-        result = num1 / num2
-    else:
-        result = num1 / num2 if num2 != 0 else "Cannot devided by zero"
-
-    st.write(f"Result: {result}")
-    
-
+if st.button("Show my profile"):
+    st.markdown(f""" # Profile Information
+    **Name:** {name},
+    **Age:** {age},
+    **Bio:**
+    {bio}
+    """)
