6f1b718e79f416b10d059b701292f8f83ebaafd1
shouq <shouqali.v@gmail.com>
2025-03-12 16:43:29 +0300

    change the label

--- app2.py
+++ app2.py
@@ -1,25 +1,4 @@
 import streamlit as st 
 
-st.title("Simple Calculator")
-
-num1 = st.number_input('Enter first number', value= 0.0)
-num2 = st.number_input('Enter second number', value= 0.0)
-
-operation = st.selectbox("choose what you want to do with the 2 numbers entered",["+","-","*","/"])
-
-# Calculate button
-if st.button("Calculate"):
-    if operation == "+":
-        result = num1 + num2
-    elif operation == "-":
-        result = num1 - num2
-    elif operation == "*":
-        result = num1 * num2
-    elif operation == "/":
-        result = num1 / num2
-    else:
-        result = num1 / num2 if num2 != 0 else "Cannot devided by zero"
-
-    st.write(f"Result: {result}")
-    
+st.title("Personal Information Form")
 
