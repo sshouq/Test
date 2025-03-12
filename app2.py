76b0aefa87eddad84c0b9ae8994d6dab56f37310
shouq <shouqali.v@gmail.com>
2025-03-12 16:38:54 +0300

    add culcalter

--- app2.py
+++ app2.py
@@ -7,3 +7,19 @@ num2 = st.number_input('Enter second number', value= 0.0)
 
 operation = st.selectbox("choose what you want to do with the 2 numbers entered",["+","-","*","/"])
 
+# Calculate button
+if st.button("Calculate"):
+    if operation == "+":
+        result = num1 + num2
+    elif operation == "-":
+        result = num1 - num2
+    elif operation == "*":
+        result = num1 * num2
+    elif operation == "/":
+        result = num1 / num2
+    else:
+        result = num1 / num2 if num2 != 0 else "Cannot devided by zero"
+
+    st.write(f"Result: {result}")
+    
+
